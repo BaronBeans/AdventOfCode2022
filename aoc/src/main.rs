de3cb460bc37c597ff4d74a61904501fb1f35bd7
Sean Barnard <sean.barnard.94@hotmail.com>
2022-12-05 11:26:21 +0000

    Better

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,4 +1,8 @@
-use std::{io, process::Command};
+use std::{
+    fs::File,
+    io::{self, Read},
+    process::Command,
+};
 
 fn main() {
     println!("Enter day:");
@@ -6,11 +10,19 @@ fn main() {
     let stdin = io::stdin();
     stdin.read_line(&mut day_input).unwrap_or_default();
 
+    let path = format!("../d{}", day_input.trim());
     Command::new("cargo")
         .arg("new")
-        .arg(format!("../d{}", day_input.trim()))
+        .arg(&path)
         .output()
         .expect("something went wrong while creating the template");
+
+    File::create(format!("{}/input.txt", &path.trim())).unwrap();
+
+    let mut file = File::open(format!("{}/src/main.rs", &path.trim())).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("{}", &contents);
 
     std::process::exit(exitcode::OK);
 }
